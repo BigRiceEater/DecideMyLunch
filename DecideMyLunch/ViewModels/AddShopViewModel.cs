7886333a35d906dd43168705ae46e6269695eb38
Big-Rice-Eater <34292181+BigRiceEater@users.noreply.github.com>
2018-04-21 22:43:27 +0800

    Add ShopViewModel base class

--- DecideMyLunch/ViewModels/AddShopViewModel.cs
+++ DecideMyLunch/ViewModels/AddShopViewModel.cs
@@ -13,48 +13,12 @@ using DecideMyLunch.Models;
 
 namespace DecideMyLunch.ViewModels
 {
-    public class AddShopViewModel : INotifyPropertyChanged
+    public class AddShopViewModel : ShopViewModel
     {
-
-        private Restaurant _shop;
-        public Restaurant Shop
-        {
-            get { return _shop; }
-            set { _shop = value; OnPropertyChanged(nameof(Shop)); }
-        }
-
         public AddShopCommand AddShopCommand { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        public AddShopViewModel(IDataStore dataStore) : base (dataStore)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private IDataStore _data;
-
-        //private DidInsertRestaurant _didInsertRestaurant;
-
-        //public DidInsertRestaurant DidInsertRestaurant
-        //{
-        //    get { return _didInsertRestaurant; }
-        //    set
-        //    {
-        //        if (_data != null)
-        //        {
-        //            _data.DidInsertRestaurant = value;
-        //        }
-        //    }
-        //}
-
-        public AddShopViewModel(IDataStore dataStore)
-        {
-            Shop = new Restaurant();
-
-            _data = dataStore;
-
             AddShopCommand = new AddShopCommand(this);
         }
 
---
+++ DecideMyLunch/ViewModels/ShopViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DecideMyLunch.Annotations;
+using DecideMyLunch.Interfaces;
+using DecideMyLunch.Models;
+
+namespace DecideMyLunch.ViewModels
+{
+    public abstract class ShopViewModel : INotifyPropertyChanged
+    {
+        private Restaurant _selectedShop;
+        public Restaurant SelectedShop
+        {
+            get => _selectedShop;
+            set { _selectedShop = value; OnPropertyChanged(nameof(SelectedShop)); }
+        }
+
+        private ICommand _cancelCommand;
+        public ICommand CancelCommand
+        {
+            get => _cancelCommand;
+            set { _cancelCommand = value; OnPropertyChanged(nameof(CancelCommand));}
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected IDataStore _data;
+        public ShopViewModel(IDataStore dataStore)
+        {
+            _data = dataStore;
+            _selectedShop = new Restaurant();
+        }
+    }
+}
